2b380ceed701b2e8c1670d0c3bf5c120aed7c61d
alehoppai <aleh_belski@outlook.com>
2023-10-22 20:56:08 +0200

    Infra: removing console.log, changing html desc

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import { MainScene } from "./scene/MainScene";
 import { handleResize } from "./utils/handleResize";
 
-console.log("Aleh Belski - Software Developer CV");
-
 const mainScene = new MainScene();
 handleResize(mainScene.Camera, mainScene.Renderer);
